dd413b9924e428986e9f1cabdcaf90eb77ea1cd0
Retch <retch100@gmail.com>
2021-09-29 20:54:37 +0200

    changed version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setup(
     name='unofficialkeepsolidvpn',
-    version='0.0.1',
+    version='0.0.2',
     description='unofficial keepsolid wireguard fetcher module',
     long_description=open('README.md').read(),
     url='https://github.com/Retch/keepsolid-vpn-wg-fetch',
